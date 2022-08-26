443a42b021b2aa6737e95d4ba5692cfeaed5bd0b
sh-leonzhang <112214327+sh-leonzhang@users.noreply.github.com>
2022-08-27 06:52:41 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,4 @@
-input.onButtonPressed(Button.A, function () {
-    if (数 % 2 == 0) {
-        basic.clearScreen()
-        basic.showString("" + 数 + "-" + "2" + "=" + (数 - 2))
-    } else {
-        if (数 % 2 == 1) {
-            basic.clearScreen()
-            basic.showString("" + 数 + "-" + "1" + "=" + (数 - 1))
-        }
-    }
+input.onGesture(Gesture.Shake, function () {
+    basic.showNumber(randint(0, 10))
 })
-input.onButtonPressed(Button.B, function () {
-    basic.clearScreen()
-    数 = randint(0, 100)
-    while (true) {
-        basic.showNumber(数)
-    }
-})
-let 数 = 0
 basic.showIcon(IconNames.Happy)
